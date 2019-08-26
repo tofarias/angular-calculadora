15d73495b5615b40b84ab6634fac2f0f3a2fb100
tofarias <tiago.farias.poa@gmail.com>
2019-08-26 08:14:10 -0300

    Add implementando componente

--- calculadora/src/app/calculadora/components/calculadora.component.ts
+++ calculadora/src/app/calculadora/components/calculadora.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 
 import { CalculadoraService } from '../services/calculadora.service';
+import { isGeneratedFile } from '@angular/compiler/src/aot/util';
+import { isNull } from 'util';
 
 @Component({
   selector: 'app-calculadora',
@@ -9,10 +11,83 @@ import { CalculadoraService } from '../services/calculadora.service';
 })
 export class CalculadoraComponent implements OnInit {
 
+private numero1: string
+private numero2: string
+private resultado: number
+private operacao: string
+
   constructor(
     private calduladoraService: CalculadoraService) { }
 
   ngOnInit() {
+    this.limpar()
   }
 
+  limpar(): void{
+    this.numero1 = '0'
+    this.numero2 = null
+    this.resultado = null
+    this.operacao = null
+  }
+
+  adicionarNumero(numero: string) : void
+  {
+    if( this.operacao === null )
+      this.numero1  = this.concatenarNumero(this.numero1, numero)
+    {
+      this.numero2  = this.concatenarNumero(this.numero2, numero)
+    }
+  }
+
+  /**
+   *
+   * @param numAtual Retorna o valor concatenado. Trata o separador decimal
+   * @param numConcat
+   */
+  concatenarNumero(numAtual: string, numConcat: string): string
+  {
+    if( numAtual ==='0' ){
+      numAtual = ''
+    }
+
+    if( numConcat === '.' && numAtual === '' ){
+      return '0.'
+    }
+
+    if ( numConcat === '.' && numAtual.indexOf('.') > -1 ){
+      return numAtual
+    }
+
+    return numAtual + numConcat
+  }
+
+  definirOperacao(operacao: string): void{
+    if(this.operacao === null){
+      this.operacao = operacao
+      return
+    }
+
+    if( this.numero2 !== null ){
+      this.resultado = this.calduladoraService.calcular(
+        parseFloat(this.numero1),
+        parseFloat(this.numero2),
+        this.operacao
+      )
+
+      this.operacao = operacao
+      this.numero1 = this.resultado.toString()
+      this.numero2 = null
+      this.resultado = null
+    }
+  }
+
+  get Display(): string{
+    if( this.resultado !== null ){
+      return this.resultado.toString()
+    }
+    if(this.numero2 !== null){
+      return this.numero2
+    }
+    return this.numero1
+  }
 }
